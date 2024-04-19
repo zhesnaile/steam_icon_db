c2dba2e792ba29bddfc627637d525de737457b81
zhesnaile <carlosojar@gmail.com>
2024-04-19 13:13:24 +0200

    [MOD] casted json response into a HashMap

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,27 @@
-use std::process::{Command, Stdio};
+use axum::{routing::get, Router};
 use std::io::{self, Write};
-use axum::{
-    routing::get,
-    Router,
-};
+use std::process::{Command, Stdio};
+
 use reqwest::Client;
+use serde::{Deserialize, Serialize};
 use serde_json;
+use std::collections::HashMap;
 
+#[derive(Debug, Deserialize, Serialize)]
+struct App {
+    appid: u32,
+    name: String,
+}
 
+#[derive(Debug, Deserialize, Serialize)]
+struct AppList {
+    apps: Vec<App>,
+}
 
-
+#[derive(Debug, Deserialize, Serialize)]
+struct Response {
+    applist: AppList,
+}
 
 fn call_steamcmd() -> Result<(), Box<dyn std::error::Error>> {
     let child = Command::new("steamcmd")
@@ -40,8 +52,15 @@ async fn request_game_id_list() -> Result<(), Box<dyn std::error::Error>> {
     const STEAM_GET_APP_LIST_URL: &str = "https://api.steampowered.com/ISteamApps/GetAppList/v2/";
     match reqwest::get(STEAM_GET_APP_LIST_URL).await {
         Ok(resp) => {
-            let json: serde_json::Value = resp.json().await?;
-            println!("{:?}", json);
+            let json: Response = resp.json().await?;
+            let game_map: HashMap<u32, String> = json
+                .applist
+                .apps
+                .into_iter()
+                .map(|app| (app.appid, app.name))
+                .collect();
+
+            println!("{:?}", game_map);
         }
         Err(err) => {
             println!("Reqwest Error: {}", err);
@@ -51,19 +70,15 @@ async fn request_game_id_list() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 async fn http_router_main() -> Result<(), Box<dyn std::error::Error>> {
-    let app = Router::new().route("/", get(|| async {
-        "Hello World!"
-    }));
+    let app = Router::new().route("/", get(|| async { "Hello World!" }));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     println!("Running on port :{}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
-    
 
     Ok(())
 }
-
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {

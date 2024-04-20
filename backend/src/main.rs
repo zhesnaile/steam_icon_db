47f39c965526b4af76a20e8d23162680a2fd00c0
zhesnaile <carlosojar@gmail.com>
2024-04-20 17:32:18 +0200

    [ADD] secondary thread for timed updates of game list

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,20 @@
 mod api;
 mod steam_queries;
 
-use api::router::http_router_main;
+use tokio::time;
+use crate::api::router::http_router_main;
+use crate::steam_queries::gamelist::request_game_id_list;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //request_game_id_list().await.unwrap();
+    tokio::spawn(async move {
+        let mut interval = time::interval(time::Duration::from_secs(60));
+        loop {
+            interval.tick().await;
+            request_game_id_list().await;
+        }
+    });
+
     crate::api::router::http_router_main().await.unwrap();
 
     Ok(())

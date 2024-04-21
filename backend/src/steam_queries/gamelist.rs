bc17f75e59551ca9802dcea943ac2d88a3d930e8
zhesnaile <carlosojar@gmail.com>
2024-04-21 14:55:24 +0200

    [MOD] made request_game_id_list return a Result<Vec<SteamApp>>

--- backend/src/steam_queries/gamelist.rs
+++ backend/src/steam_queries/gamelist.rs
@@ -1,9 +1,8 @@
-use reqwest::Error;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use anyhow::{Context, Result};
 
 #[derive(Debug, Deserialize, Serialize)]
-struct App {
+pub struct SteamApp {
     appid: u32,
     name: String,
     linuxclienticon: Option<String>,
@@ -11,32 +10,21 @@ struct App {
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-struct AppList {
-    apps: Vec<App>,
+struct SteamAppList {
+    apps: Vec<SteamApp>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-struct Response {
-    applist: AppList,
+struct SteamAPIResponse {
+    applist: SteamAppList,
 }
 
-pub async fn request_game_id_list() -> Result<(), Box<dyn std::error::Error>> {
+pub async fn request_game_id_list() -> Result<Vec<SteamApp>> {
     const STEAM_GET_APP_LIST_URL: &str = "https://api.steampowered.com/ISteamApps/GetAppList/v2/";
-    match reqwest::get(STEAM_GET_APP_LIST_URL).await {
-        Ok(resp) => {
-            let json: Response = resp.json().await?;
-            let game_map: HashMap<u32, String> = json
-                .applist
-                .apps
-                .into_iter()
-                .map(|app| (app.appid, app.name))
-                .collect();
+    let gamelist = reqwest::get(STEAM_GET_APP_LIST_URL)
+    .await.context("Failed get request")?
+    .json::<SteamAPIResponse>().await.context("Failed to get JSON body")?
+    .applist.apps;
 
-            println!("{:?}", game_map);
-        }
-        Err(err) => {
-            println!("Reqwest Error: {}", err);
-        }
-    }
-    Ok(())
+    Ok(gamelist)
 }

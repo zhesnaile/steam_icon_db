691964967a4be6fdc5f269f686ccc24f8614c8f2
zhesnaile <carlosojar@gmail.com>
2024-04-23 00:46:41 +0200

    [ADD] added unit test for steamcmd::get_client_icons

--- backend/src/steam_queries/steamcmd.rs
+++ backend/src/steam_queries/steamcmd.rs
@@ -1,8 +1,8 @@
+use anyhow::Result;
 use std::io::{self, Write};
 use std::process::{Command, Stdio};
-use anyhow::Result;
 
-fn get_client_icons(appid: u32) -> Result<()> {
+fn get_client_icons(appid: u32) -> Result<(Option<String>, Option<String>)> {
     let child = Command::new("steamcmd")
         .arg("+app_info_print")
         .arg(appid.to_string())
@@ -16,13 +16,35 @@ fn get_client_icons(appid: u32) -> Result<()> {
 
     if output.status.success() {
         println!("SteamCMD command executed successfully!");
-        io::stdout().write_all(&output.stdout).unwrap();
+        let d = String::from_utf8(output.stdout).unwrap();
+        for line in d.lines() {
+            println!("{}", line);
+        }
     } else {
         println!(
             "SteamCMD command failed with exit status: {}",
             output.status
         );
     }
+    let clienticon = Some(String::from("d9157d92d45689e1ec92aea00980fcfad0ce977e"));
+    let linuxclienticon: Option<String> = None;
 
-    Ok(())
+    Ok((clienticon, linuxclienticon))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_client_icons() {
+        const APPID: u32 = 590380;
+
+        let test_clienticon = Some(String::from("d9157d92d45689e1ec92aea00980fcfad0ce977e"));
+        let test_linuxclienticon: Option<String> = None;
+
+        if let Ok(result) = get_client_icons(APPID) {
+            assert_eq!((test_clienticon, test_linuxclienticon), result)
+        }
+    }
 }

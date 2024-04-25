ff0e6bbdf1a970efc7f6323d611258d951080028
zhesnaile <carlosojar@gmail.com>
2024-04-25 16:39:09 +0200

    [MOD] filtered out output preceding the actual vdf text of the given appid

--- backend/src/steam_queries/steamcmd.rs
+++ backend/src/steam_queries/steamcmd.rs
@@ -1,8 +1,21 @@
 use anyhow::Result;
+use serde::{Deserialize, Serialize};
 use std::io::{self, Write};
 use std::process::{Command, Stdio};
 
-fn get_client_icons(appid: u32) -> Result<(Option<String>, Option<String>)> {
+#[derive(Deserialize, Debug, Serialize)]
+struct AppInfo {
+    #[serde(rename = "Common")]
+    common: Common,
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+struct Common {
+    clienticon: Option<String>,
+    linuxclienticon: Option<String>,
+}
+
+pub fn get_client_icons(appid: u32) -> Result<(Option<String>, Option<String>)> {
     let child = Command::new("steamcmd")
         .arg("+app_info_print")
         .arg(appid.to_string())
@@ -16,10 +29,30 @@ fn get_client_icons(appid: u32) -> Result<(Option<String>, Option<String>)> {
 
     if output.status.success() {
         println!("SteamCMD command executed successfully!");
+
         let d = String::from_utf8(output.stdout).unwrap();
+
+        let mut steam_app_vec = Vec::new();
+        let mut pattern_found = false;
+
+        let pattern = format!("\"{appid}\"");
+
         for line in d.lines() {
-            println!("{}", line);
+            if !pattern_found {
+                if line == pattern {
+                    pattern_found = true;
+                    steam_app_vec.push(line.to_string());
+                }
+            } else {
+                steam_app_vec.push(line.to_string());
+            }
         }
+
+        let steam_app_vec = steam_app_vec.join("\n");
+
+        let app_info: AppInfo = keyvalues_serde::from_str(&steam_app_vec)?;
+
+        println!("Appid info: {app_info:#?}");
     } else {
         println!(
             "SteamCMD command failed with exit status: {}",

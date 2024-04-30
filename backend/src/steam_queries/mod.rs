39060f82a75004cda934729210799d74d408e402
zhesnaile <carlosojar@gmail.com>
2024-04-30 23:31:38 +0200

    [DEL] removed unused var

--- backend/src/steam_queries/mod.rs
+++ backend/src/steam_queries/mod.rs
@@ -1,9 +1,6 @@
 use anyhow::Result;
 
-use self::{
-    gamelist::{request_game_id_list, SteamApp},
-    steamcmd::get_client_icons,
-};
+use self::{gamelist::request_game_id_list, steamcmd::get_client_icons};
 pub mod gamelist;
 pub mod steamcmd;
 
@@ -11,8 +8,8 @@ pub async fn update_game_db() -> Result<()> {
     let game_list = request_game_id_list().await?;
 
     for steamapp in game_list {
-        if (!steamapp.name.trim().is_empty()) {
-            let x = match get_client_icons(steamapp.appid).await {
+        if !steamapp.name.trim().is_empty() {
+            match get_client_icons(steamapp.appid).await {
                 Ok(val) => println!("Appid {0:?}: {1:?}", steamapp.appid, val.clienticon),
                 Err(err) => eprintln!("{}", err),
             };

3a2555d77842a797cb472ab24ed078eaed3aa70b
zhesnaile <carlosojar@gmail.com>
2024-04-30 23:14:05 +0200

    [MOD] switched to devenv

--- flake.nix
+++ flake.nix
@@ -6,11 +6,16 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-parts.url = "github:hercules-ci/flake-parts";
     cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    devenv.url = "github:cachix/devenv";
   };
 
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" ];
+
+      imports = [
+        inputs.devenv.flakeModule
+      ];
 
       perSystem = { self', pkgs, system, ... }:
       let
@@ -29,26 +34,42 @@
           overlays = [ inputs.cargo2nix.overlays.default (import inputs.rust-overlay) ];
         };
 
-        # Development Shell
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            rust-bin.stable.${rustVersion}.default
-            cargo-edit
-            cargo-watch
-            rust-analyzer
-            clippy
-            rustfmt
+        devenv.shells.default = {
+          languages = {
+            rust.enable = true;
+            javascript = {
+                enable = true;
+                corepack.enable = true;
+
+                directory = "frontend";
+                pnpm = {
+                    enable = true;
+                    install.enable = true;
+                };
+            };
+          };
+          packages = with pkgs; [
             steamcmd
             openssl.dev
+            pkg-config
           ];
-          nativeBuildInputs = [pkgs.pkg-config];
+
+          scripts = {
+            build-backend.exec = "cd backend && cargo build --release";
+            run-backend.exec = "cd backend && cargo run --release";
+            test-backend.exec = "cd backend && cargo test --release";
+          };
+
+          processes = {
+            backend.exec = "cd backend && cargo run";
+          };
+          
+          services.redis.enable = true;
         };
 
         # Packages
         packages = rec {
-            steam_icon_db_api = (rustPkgs.workspace.steam_icon_db_api {}).bin.overrideAttrs (oldAttrs: {
-              buildInputs = (oldAttrs.buildInputs or []) ++ [ pkgs.steamcmd ];
-            });
+            steam_icon_db_api = (rustPkgs.workspace.steam_icon_db_api {}).bin;
             default = steam_icon_db_api;
         };
 

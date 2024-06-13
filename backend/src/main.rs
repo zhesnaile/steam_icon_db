88d60116fd6464cd57d896a0e4ba7af5deecd86a
zhesnaile <carlosojar@gmail.com>
2024-06-13 13:11:54 +0200

    [MOD] abstracted background task instantiation to a function

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,18 +6,20 @@ use crate::steam_queries::update_game_db;
 use anyhow::Result;
 use tokio::time;
 
+async fn update_game_db_task() {
+    let mut interval = time::interval(time::Duration::from_secs(60));
+    loop {
+        interval.tick().await;
+        match update_game_db().await {
+            Ok(_) => (),
+            Err(_err) => (),
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    tokio::spawn(async move {
-        let mut interval = time::interval(time::Duration::from_secs(60));
-        loop {
-            interval.tick().await;
-            match update_game_db().await {
-                Ok(_) => (),
-                Err(_err) => (),
-            }
-        }
-    });
+    tokio::spawn(update_game_db_task());
 
     http_router_main().await.unwrap();
 
